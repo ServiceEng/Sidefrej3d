2cb35d06f2b31dc4d6e60e885772c548cc266df1
ServiceEng <VMMartinez@amstedrail.com>
2022-12-12 09:18:06 -0600

    commit 1

---
+++ main.js
@@ -0,0 +1,26 @@
+
+    function getOrbit(){
+      const modelViewer = document.querySelector('#canon');
+      console.log(`recordOrbit ${modelViewer.getCameraOrbit()}`);
+    }
+    function setOrbit(orbit){
+      const modelViewer = document.querySelector('#canon');
+      modelViewer.cameraOrbit = orbit;
+      console.log('setOrbit');
+    }
+
+    // Handles loading the events for <model-viewer>'s slotted progress bar
+const onProgress = (event) => {
+  const progressBar = event.target.querySelector('.progress-bar');
+  const updatingBar = event.target.querySelector('.update-bar');
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
+    progressBar.classList.add('hide');
+  } else {
+    progressBar.classList.remove('hide');
+    if (event.detail.totalProgress === 0) {
+      event.target.querySelector('.center-pre-prompt').classList.add('hide');
+    }
+  }
+};
+document.querySelector('model-viewer').addEventListener('progress', onProgress);
